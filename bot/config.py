168452d4346872be0823479ab0966e5033a324b8
Ruined Soul <76905260+ruined-soul@users.noreply.github.com>
2024-08-07 09:51:25 +0530

    Update config.py

--- bot/config.py
+++ bot/config.py
@@ -1,6 +1,6 @@
 import os
 
-bot_token = "7452696572:AAH9HPL5iAJmpujno_FPJpK-KfOGFRny5j>
+bot_token = "7452696572:AAH9HPL5iAJmpujno_FPJpK-KfOGFRny5j"
 developer_id = 7329859398
 log_channel_id = -1002184975232
-approved_groups = [-1002162035881]  # Add your approved gr>
+approved_groups = [-1002162035881]  # Add your approved group chat IDs here
